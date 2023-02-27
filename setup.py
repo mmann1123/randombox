a6a514cca89b0f56ef5528495cfe68db3c84765b
Michael Mann <mmann1123@gmail.com>
2023-02-26 23:33:12 -0500

    git set

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="randombox",
     version="0.1",
     description="Create random points inside of a polygon and create square polygons around each point",
-    url="",  # github url
+    url="https://github.com/mmann1123/randombox.git",  # github url
     author="Mike Mann",
     author_email="mmann1123@gwu.edu",
     license="MIT",
